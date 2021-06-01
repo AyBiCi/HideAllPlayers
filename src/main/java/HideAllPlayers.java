92d4866c3dab1056991ac4384c515c50a5848aa4
AyBiCi <rymcymcym100@gmail.com>
2021-06-01 11:35:04 +0200

    Implementing...

--- src/main/java/HideAllPlayers.java
+++ src/main/java/HideAllPlayers.java
@@ -1,10 +1,57 @@
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
-public class HideAllPlayers extends JavaPlugin {
+public class HideAllPlayers extends JavaPlugin implements Listener {
+
+    private Set<Player> playersThatHideOthers = new HashSet<>();
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(command.getName().equalsIgnoreCase("hideplayers"))
+            hideAllPlayersFor((Player) sender);
+        else
+            showAllPlayersFor((Player) sender);
+        return false;
+    }
+
+    public void hideAllPlayersFor(Player player) {
+        playersThatHideOthers.add(player);
+        for(Player p : Bukkit.getOnlinePlayers()) {
+            player.hidePlayer(this, p);
+        }
+    }
+
+    public void showAllPlayersFor(Player player) {
+        playersThatHideOthers.remove(player);
+        for(Player p : Bukkit.getOnlinePlayers()) {
+            player.showPlayer(this, p);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+        Player joinedPlayer = event.getPlayer();
+        for(Player player : playersThatHideOthers) {
+            player.hidePlayer(this, joinedPlayer);
+        }
+    }
 
     //For MockBukkit
     public HideAllPlayers()
--- src/test/java/HideAllPlayersTest.java
+++ src/test/java/HideAllPlayersTest.java
@@ -1,3 +1,50 @@
-public class HideAllPlayersTest {
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import org.bukkit.entity.Player;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HideAllPlayersTest {
+    private ServerMock server;
+    private HideAllPlayers plugin;
+    private Player player1;
+    private Player player2;
+    @Before
+    public void setUp()
+    {
+        server = MockBukkit.mock();
+        plugin = (HideAllPlayers) MockBukkit.load(HideAllPlayers.class);
+        player1 = server.addPlayer();
+        player2 = server.addPlayer();
+    }
+
+    @After
+    public void tearDown()
+    {
+        MockBukkit.unmock();
+    }
+
+    @Test
+    public void testCommands() {
+        player1.performCommand("hideplayers");
+        assertFalse(player1.canSee(player2));
+        player1.performCommand("showplayers");
+        assertTrue(player1.canSee(player2));
+    }
+
+    @Test
+    public void onNewPlayerEnter(){
+        player1.performCommand("hideplayers");
+        player2 = server.addPlayer();
+        assertFalse(player1.canSee(player2));
+    }
+
+    @Test
+    public void onPlayerExitWithHidingPlayersOn(){
+
+    }
 }
